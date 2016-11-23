331cb23e7e911b19e264d0480538c5b9420e084d
Abhijeet Shiralkar <Abhijeet.Shiralkar@lntinfotech.com>
2016-11-23 18:17:43 +0530

    Added date to return statement

--- gs-circuit-breaker-complete/bookstore/src/main/java/hello/BookstoreApplication.java
+++ gs-circuit-breaker-complete/bookstore/src/main/java/hello/BookstoreApplication.java
@@ -1,20 +1,26 @@
 package hello;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
 public class BookstoreApplication {
 
-  @RequestMapping(value = "/recommended")
-  public String readingList(){
-    return "Spring in Action (Manning), Cloud Native Java (O'Reilly), Learning Spring Boot (Packt)";
-  }
+	@RequestMapping(value = "/recommended")
+	public String readingList() {
+		final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return "Spring in Action (Manning), Cloud Native Java (O'Reilly), Learning Spring Boot (Packt) recommended on "
+				+ dateFormat.format(new Date());
+	}
 
-  public static void main(String[] args) {
-    SpringApplication.run(BookstoreApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
 }
